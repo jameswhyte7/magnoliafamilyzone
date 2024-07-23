03ad239cb30cf35e1448d42998c57951188fe46b
jameswhyte7 <boikhristmas@gmail.com>
2024-07-23 01:23:39 +0000

    fifth push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { FaPhone, FaInstagram } from 'react-icons/fa';
 
 const Header = () => (
   <header className="bg-primary text-white text-center py-3">
-    <div className="header-content d-flex justify-content-center align-items-center">
-      <FaInstagram size="24" className="mr-2"/>
-      <h1>magnoliafamilyzone</h1>
+    <div className="header-content d-flex">
+      <FaInstagram size="30" className="mr-2"/>
+      <h1 className="text-center">magnoliafamilyzone</h1>
     </div>
     <div className="mt-2 d-flex justify-content-center align-items-center">
       <FaPhone size="20" className="mr-2" /> 
