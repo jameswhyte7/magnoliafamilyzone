a3e4555d92d859fb8fecc11b3a86f8c0766d561b
jameswhyte7 <boikhristmas@gmail.com>
2024-07-28 18:09:09 +0000

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="bg-light d-flex justify-content-center align-items-center py-4">
     <div className="d-flex justify-content-center align-items-center w-100">
-      <p className="m-0">Powered by AG Software - 0505887272</p>
+      <p className="m-0" style={{color: "#858D86"}}>Powered by AG Software - 0505887272</p>
     </div>
   </footer>
 );
